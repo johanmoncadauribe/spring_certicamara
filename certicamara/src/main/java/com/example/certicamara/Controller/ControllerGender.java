de773f5fb27b1e089d6151a4843c15f4264bf40f
jhoanmoncada <jhoanmoncada@wposs.com>
2022-02-03 17:07:05 -0500

    Servicio GET:
    - Se crea consumo de servicio externo Gender, se envia nombre por url y el servicio responde a que genero pertenece (hombre o mujer).
    - Se guarda en una base de datos las veces que se ha consumido el servicio con su respectiva fecha.

--- certicamara/src/main/java/com/example/certicamara/Controller/ControllerGender.java
+++ certicamara/src/main/java/com/example/certicamara/Controller/ControllerGender.java
@@ -1,5 +1,11 @@
 package com.example.certicamara.Controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +15,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.certicamara.Utils;
+import com.example.certicamara.Dto.GenderDTO;
+import com.example.certicamara.Model.modelGender;
 import com.example.certicamara.Repository.RepoGenderize;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 @RestController
 @RequestMapping(path="gender")
@@ -22,7 +33,44 @@ public class ControllerGender {
 	//prueba
 		@GetMapping(path="/apiGender/{name}")
 		public @ResponseBody Map<String, Object> getGender (@PathVariable String name){
+			if(name.isBlank()||name.isEmpty()||name==null||name.equalsIgnoreCase("null")) {
+				return Utils.respuesta(false, "Nombre incorrecto", null);
+			}
 			
-			return null;
+			StringBuilder response = new StringBuilder();
+			
+			try {
+					modelGender mod=new modelGender();
+					repogenderize.save(mod);
+			}catch(Exception e) {
+				  return Utils.respuesta(false, "Error desconocido", null);
+			}
+				
+			try {
+				URL url = new URL ("https://api.genderize.io?name="+name);
+				HttpURLConnection con = (HttpURLConnection)url.openConnection();
+				con.getRequestMethod();
+				con.setRequestProperty("Accept", "application/json");
+				try(BufferedReader br = new BufferedReader(
+						  new InputStreamReader(con.getInputStream(), "utf-8"))) {
+						    String responseLine = null;
+						    while ((responseLine = br.readLine()) != null) {
+						       response.append(responseLine);
+						    }
+				}
+				JsonObject jsonObject = new JsonParser().parse(response.toString()).getAsJsonObject();
+				GenderDTO gen=new GenderDTO();
+				gen.setName(jsonObject.get("name").getAsString());
+				gen.setGender(jsonObject.get("gender").getAsString());
+				gen.setProbability(jsonObject.get("probability").getAsString());
+				gen.setCount(jsonObject.get("count").getAsString());
+				return Utils.respuesta(true, "succes", gen);
+			}catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				return Utils.respuesta(false, "Error en la consulta", null);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				return Utils.respuesta(false, "Error en la consulta", null);
+			}
 		}
 }
